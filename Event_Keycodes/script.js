e967103300ed8ccd96cb6cbcb5f00523f4c2dcc0
Joey <160741266+JoeyNMason@users.noreply.github.com>
2024-04-25 16:05:14 +0100

    Add files via upload

---
+++ Event_Keycodes/script.js
@@ -0,0 +1,22 @@
+const insert = document.getElementById("insert")
+
+window.addEventListener("keydown", (event) => {
+    insert.innerHTML = `        
+    
+    <div class="key">
+        ${event.key === " " ? "Space" : event.key} 
+        <small>event.key</event></small>
+    </div>
+
+    <div class="key">
+        ${event.keyCode}
+        <small>event.keycode</small>
+    </div>
+
+    <div class="key">
+        ${event.code}
+        <small>event.code</small>
+    </div>
+    
+    `
+})
