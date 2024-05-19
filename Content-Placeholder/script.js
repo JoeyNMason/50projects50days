4a6dc1658ab52e6cd40a25447e4fcc0a52d674b7
Joey <160741266+JoeyNMason@users.noreply.github.com>
2024-05-19 18:34:49 +0100

    Add files via upload

--- Content-Placeholder/script.js
+++ Content-Placeholder/script.js
@@ -1,6 +1,35 @@
 const header = document.getElementById("header")
 const title = document.getElementById("title")
 const excerpt = document.getElementById("excerpt")
-const p = document.getElementById("header")
-const header = document.getElementById("header")
-const header = document.getElementById("header")
+const profile_img = document.getElementById("profile_img")
+const author = document.getElementById("author")
+const date = document.getElementById("date")
+
+const animated_bgs = document.querySelectorAll(".animated-bg")
+const animated_bg_texts = document.querySelectorAll(".animated-bg-text")
+
+setTimeout(getData, 1250)
+
+function getData(){
+    
+    // card img 
+    header.innerHTML = '' 
+
+    // card title
+    title.innerHTML = ''
+
+    // excerpt
+    excerpt.innerHTML = ''
+
+    // profile image
+    profile_img.innerHTML = ''
+
+    // name
+    author.innerHTML = ''
+
+    // date
+    date.innerHTML = ''
+
+    animated_bgs.forEach((bg) => bg.classList.remove("animated-bg"))
+    animated_bg_texts.forEach((bg) => bg.classList.remove("animated-bg-text"))
+}
